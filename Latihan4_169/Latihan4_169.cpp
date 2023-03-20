6d9c4acaf79de769cd74344a70fe5547f4dfcf7d
Ryvqi <rifqikhabibi03@gmail.com>
2023-03-20 15:05:08 +0700

    membuart menu

--- Latihan4_169/Latihan4_169.cpp
+++ Latihan4_169/Latihan4_169.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
-using
+using namespace std;
+
+
+float LuasPersegi(float p, float l) {
+    return p * l;
+}
+float LuasLingkaran(float r) {
+    return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+    return 0.5 * a * t;
+}
+
+int main() {
+
+    float panjang, lebar, jejari, tinggi;
+    int pilihan;
+    do {
+        cout << "\n\n==========================" endl;
+        cout << "========== MENU ==============" endl;
+        cout << "==============================" endl;
+        cout << "1. Luas Persegi" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Luas Segitiga" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Masukan Pilihan (1/2/3/4) :";
+        cin >> pilihan;
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
